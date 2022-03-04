a758239c07cd7ad334ae44ed605560bc1a73b1a5
niallh <n.hargreaves@prospects.ac.uk>
2022-03-04 17:28:19 +0000

    fixed App.js errors and re-added dependencies

--- App.js
+++ App.js
@@ -1,20 +1,31 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
 
-export default function App() {
+import {NavigationContainer} from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+
+import SignupScreen from "./assets/screens/SignupScreen";
+import LoginScreen from "./assets/screens/LogInScreen";
+import EditPostScreen from "./assets/screens/EditPostScreen";
+import MainAppScreen from "./assets/screens/MainAppScreen";
+
+
+const Stack = createNativeStackNavigator();
+
+export const App = () => {
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <NavigationContainer>
+        <Stack.Navigator
+            screenOptions={{
+              headerShown: false
+            }}>
+          <Stack.Screen name={"Login"} component={LoginScreen} />
+          <Stack.Screen name={"Main App"} component={MainAppScreen} />
+          <Stack.Screen name={"Sign Up"} component={SignupScreen} />
+          <Stack.Screen name={"Edit Post"} component={EditPostScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
