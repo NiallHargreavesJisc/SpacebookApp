eb3f054eab5549d9b313ad87cd665d14c74f3baa
niallh <n.hargreaves@prospects.ac.uk>
2022-03-13 17:57:14 +0000

    added header to stack screen

--- App.js
+++ App.js
@@ -19,10 +19,7 @@ export const App = () => {
 
   return (
       <NavigationContainer>
-        <Stack.Navigator
-            screenOptions={{
-              headerShown: false
-            }}>
+        <Stack.Navigator>
           <Stack.Screen name={"Login"} component={LoginScreen} />
           <Stack.Screen name={"Main App"} component={MainAppScreen} />
           <Stack.Screen name={"Sign Up"} component={SignupScreen} />
